e556623d78253eefedb61228a4415e06551b4648
mbaker <mekenna.drabner@gmail.com>
2024-11-15 13:49:38 -0700

    completed weatherRoutes. Created Post request to retrieve weather, Got weather data from city name and saved to search history, then got search history

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -1,17 +1,53 @@
+import { Router } from 'express';
+const router = Router();
 
+import HistoryService from '../../service/historyService';
+import WeatherService from '../../service/WeatherService';
 
 // TODO: POST Request with city name to retrieve weather data
 
+router.post('/', async (req, res) => {
+  const cityName = req.body.cityName;
 
+  if (!cityName) {
+    return res.status(400).json({ error: 'City name is required' });
+  }
 
-  // TODO: GET weather data from city name
+  try {
+    console.log(`Fetching weather data for city: ${cityName}`);
 
+    // TODO: GET weather data from city name
 
-  // TODO: save city to search history
+    const weatherData = await WeatherService.getWeatherforCity(cityName);
+    console.log('Weather data retrieved', weatherData);
 
+    // TODO: Save city to search history
+    
+    await HistoryService.addCity(cityName);
+    console.log(`${cityName} added to search history`);
+
+    return res.status(200).json(weatherData);
+  } catch (error) {
+    console.error('Error fetching weather data', error);
+    return res.status(500).json({ error: 'Error fetching weather data' });
+  }
+});
 
 // TODO: GET search history
 
+router.get('/history', async (_req, res) => {
+  try {
+    console.log('Fetching search history');
+    const searchHistory = await HistoryService.getCities();
+    return res.json(searchHistory);
+
+  } catch (error) {
+    console.error('Error fetching search history', error);
+    return res.status(500).json({ error: 'Failed to fetch search history' });
+  }
+});
+
+export default router;
 
 
 // * BONUS TODO: DELETE city from search history
