8ebb55ec60cc04dfe50cb26de108c8c1742b8a6e
mbaker <mekenna.drabner@gmail.com>
2024-11-15 15:29:15 -0700

    edited file extensions to include .js. Built front and back end

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express';
 const router = Router();
 
-import HistoryService from '../../service/historyService';
-import WeatherService from '../../service/weatherService';
+import HistoryService from '../../service/historyService.js';
+import WeatherService from '../../service/weatherService.js';
 
+const baseURL = process.env.API_BASE_URL || ''; 
+const apiKey = process.env.API_KEY || '';
 
-// TODO: POST Request with city name to retrieve weather data
+// Create instances of each service class
+
+const historyService = new HistoryService();
+const weatherService = new WeatherService(baseURL, apiKey);
+
+// POST Request with city name to retrieve weather data
 
 router.post('/', async (req, res) => {
   const cityName = req.body.cityName;
@@ -17,38 +24,36 @@ router.post('/', async (req, res) => {
   try {
     console.log(`Fetching weather data for city: ${cityName}`);
 
-    // TODO: GET weather data from city name
+    // Get weather data from city name
+    const weatherData = await weatherService.getWeatherForCity(cityName);
+    console.log('Weather data retrieved:', weatherData);
 
-    const weatherData = await WeatherService.getWeatherforCity(cityName);
-    console.log('Weather data retrieved', weatherData);
-
-    // TODO: Save city to search history
-
-    await HistoryService.addCity(cityName);
+    // Save city to search history
+    await historyService.addCity(cityName);
     console.log(`${cityName} added to search history`);
 
     return res.status(200).json(weatherData);
   } catch (error) {
-    console.error('Error fetching weather data', error);
+    console.error('Error fetching weather data:', error);
     return res.status(500).json({ error: 'Error fetching weather data' });
   }
 });
 
-// TODO: GET search history
+// GET search history
 
 router.get('/history', async (_req, res) => {
   try {
     console.log('Fetching search history');
-    const searchHistory = await HistoryService.getCities();
+    const searchHistory = await historyService.getCities();
     return res.json(searchHistory);
-
   } catch (error) {
-    console.error('Error fetching search history', error);
+    console.error('Error fetching search history:', error);
     return res.status(500).json({ error: 'Failed to fetch search history' });
   }
 });
 
 export default router;
+
 
 
 // * BONUS TODO: DELETE city from search history
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,8 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 dotenv.config();
 
 // Import the routes
@@ -11,15 +14,21 @@ const PORT = process.env.PORT || 3001;
 
 // TODO: Serve static files of entire client dist folder //
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
 
 
 // TODO: Implement middleware for parsing JSON and urlencoded form data //
 
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // TODO: Implement middleware to connect the routes //
 
-
+import routes from './routes/index.js';
+app.use(routes);
 
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
